2830a6fa64b7790e75cc224f1cce249369801ff1
Huk-Luko <it.sluzby.hk@gmail.com>
2020-08-17 12:51:04 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const users = require('./routes/users');
+/* const users = require('./routes/users'); */
 const auth = require('./routes/auth');
 const mongoose = require('mongoose');
 const config = require('config');
@@ -27,7 +27,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/users', users);
+/* app.use('/api/users', users); */
 app.use('/api/auth',auth);
 
 const port = process.env.PORT || 3000;
